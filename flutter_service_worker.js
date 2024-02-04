68574eedcf6746570c55de9842deb99801b3f172
Karkki Jothibasu <135545035+karkki-jothibasu@users.noreply.github.com>
2024-02-05 02:11:09 +0530

    new1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -62,8 +62,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "cd4d0876095c0c5f9262a504d40ccfc7
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f5e409c40753ea4b5bf9e22e6ebd8b17",
-"/": "f5e409c40753ea4b5bf9e22e6ebd8b17",
+"index.html": "0e76450ae7bf2a41bb0065a6011c11a0",
+"/": "0e76450ae7bf2a41bb0065a6011c11a0",
 "main.dart.js": "d202e3780f04ab3d3e31c6bcd865a6ea",
 "manifest.json": "1c2171ac5283a6e3182c88e312294f81",
 "version.json": "08e7d180b1144e49e6fccec76aa70200"};
